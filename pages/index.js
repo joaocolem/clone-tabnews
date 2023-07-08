2271d920c955fef7bcfb16abb7c157ad81c5f4ae
João Colem <105292741+joaocolem@users.noreply.github.com>
2023-07-08 17:30:41 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
-    return <h1>Essa é minha primeira url de teste, ao final do curso vamos ver a pagina que cheguei, com funcionalidades!!
-        Obrigado por sempre estar ao meu lado e me apoiar, te amo
+    return <h1>Oi tudo bem
+        
     </h1>
 }
 
