c4f495133fdc1f83b2d878167ba036377296a317
João Colem <105292741+joaocolem@users.noreply.github.com>
2023-07-09 21:12:19 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
+// Exemplo usando o módulo 'mysql' para conectar ao banco de dados MySQL
 
-function LandingPage() {
-  return (
-    <div className="landing-page">
-      <h1>Bem-vindo à nossa página!</h1>
-      <p>Estamos felizes em tê-lo aqui.</p>
-      <button>Explorar</button>
-    </div>
-  );
-}
+const mysql = require('mysql');
 
-export default LandingPage;
+// Configuração da conexão com o banco de dados
+const connection = mysql.createConnection({
+  host: '172.17.0.2',
+  user: 'root',
+  password: 'root',
+  database: 'nomedobanco' // substitua 'nomedobanco' pelo nome real do banco de dados
+});
+
+// Estabelece a conexão
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+
+  console.log('Conexão bem-sucedida ao banco de dados.');
+
+  // Executa a consulta
+  connection.query('SELECT * FROM motorista', (error, results, fields) => {
+    if (error) {
+      console.error('Erro ao executar a consulta:', error);
+      return;
+    }
+
+    // Exibe os resultados
+    console.log('Resultados da consulta:');
+    console.log(results);
+
+    // Encerra a conexão
+    connection.end((err) => {
+      if (err) {
+        console.error('Erro ao encerrar a conexão:', err);
+        return;
+      }
+
+      console.log('Conexão encerrada.');
+    });
+  });
+});
