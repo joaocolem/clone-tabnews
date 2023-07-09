2641783459094fe4582f7a37b5c10736d3060718
João Colem <105292741+joaocolem@users.noreply.github.com>
2023-07-09 20:32:07 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-function Home(){
-    return <h1>Oi tudo bem
-        
-    </h1>
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1>Bem-vindo à nossa página!</h1>
+      <p>Estamos felizes em tê-lo aqui.</p>
+      <button>Explorar</button>
+    </div>
+  );
 }
 
-export default Home;
+export default LandingPage;
