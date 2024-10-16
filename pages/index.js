b62a599cab27d722aeaf2f0889b6ea516ede9b84
João Colem <105292741+joaocolem@users.noreply.github.com>
2024-10-16 13:18:50 +0000

    teste alterar pagina

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ function LandingPage() {
 
   return (
     <div className="landing-page">
-      <h1>Bem-vindo à nossa página!</h1>
+      <h1>Bem-vindo à nossa página2!</h1>
       <p>Estamos felizes em tê-lo aqui.</p>
       <button>Explorar</button>
     </div>
