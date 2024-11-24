8b91a79580a1215d2884c3db105bb66d57657bf0
João Colem <105292741+joaocolem@users.noreply.github.com>
2024-11-24 15:47:29 +0000

    a

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import axios from 'axios';
 
 function LandingPage() {
   useEffect(() => {
     fetchData();
   }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('/api/motoristas');
-      console.log(response.data);
-    } catch (error) {
-      console.error('Erro ao obter dados:', error);
-    }
-  };
 
   return (
     <div className="landing-page">
