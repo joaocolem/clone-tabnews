96efe90c0b2edff52fa25bdf2f2ffaf76bbaf8eb
João Colem <105292741+joaocolem@users.noreply.github.com>
2024-11-24 17:26:49 +0000

    teste anbtigo

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,5 @@
-import React from 'react';
-
-function LandingPage() {
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-
-  return (
-    <div className="landing-page">
-      <h1>Bem-vindo à nossa página2!</h1>
-      <p>Estamos felizes em tê-lo aqui.</p>
-      <button>Explorar</button>
-    </div>
-  );
+function Home(){
+    return <h1>Teste</h1>
 }
 
-export default LandingPage;
+export default Home;
