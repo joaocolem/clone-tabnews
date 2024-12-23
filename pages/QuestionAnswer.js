9a1fbc2d1e0199aff95d48803289a01bd1626ae6
pedrocolen <47362124+pedrocolen@users.noreply.github.com>
2024-12-23 16:05:50 -0300

    Update QuestionAnswer.js
    mudando o prompt

--- pages/QuestionAnswer.js
+++ pages/QuestionAnswer.js
@@ -29,7 +29,7 @@ export async function getGroqChatCompletion() {
             {
                 role: "user",
                 
-                content: `Elabore uma pergunta de múltipla escolha no estilo "Show do Milhão", com nível de dificuldade facil, relacionada ao tema "${randomTheme}". A pergunta deve ser simples, objetiva e clara, com opções diretas e curtas. As respostas devem ser fáceis de entender e relacionadas corretamente ao tema. Certifique-se de que a pergunta tenha a opção correta e que as respostas sejam simples e diretas. A resposta deve ser no formato: {question: '...', options: ['opção 0', 'opção 1', 'opção 2'], correct: 0}. Eu quero apenas o que está dentro de {} e não quero nada além disso.`
+                content: `Elabore uma pergunta de múltipla escolha no estilo "Show do Milhão", com nível de dificuldade fácil. A pergunta deve ser simples, objetiva e clara, com opções diretas e curtas, cada texto da opcao deve ter no maximo 3 palavras. As respostas devem ser fáceis de entender e relacionadas corretamente ao tema. Certifique-se de que a pergunta tenha a opção correta e que as respostas sejam simples e diretas. A resposta deve ser no formato: {question: '...', options: ['opção 0', 'opção 1', 'opção 2'], correct: 0}. Eu quero apenas o que está dentro de {} e não quero nada além disso.`
             },
         ],
         model: "llama3-8b-8192",
