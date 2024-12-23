444e7101ba3036bfa52a26f6228f8461463689d8
João Colem <105292741+joaocolem@users.noreply.github.com>
2024-12-23 20:30:26 +0000

    ptbr

--- pages/QuestionAnswer.js
+++ pages/QuestionAnswer.js
@@ -28,7 +28,8 @@ export async function getGroqChatCompletion() {
             {
                 role: "user",
                 
-                content: `Elabore, em brasileiro, uma pergunta de múltipla escolha no estilo "Show do Milhão", com nível de dificuldade fácil ou médio relacionada ao tema "${randomTheme}". As perguntas serão para um jogo a ser jogado em família, com pessoas de todas as idades. A pergunta deve ser simples, objetiva e clara, deve ter opções de resposta diretas e curtas, mas as respostas não devem ser tão óbvias, cada texto da opcao da resposta deve ter no maximo 3 palavras. As respostas devem ser fáceis de entender e relacionadas corretamente a pergunta e ao tema. Certifique-se de que a pergunta tenha a opção correta e que as respostas sejam simples e diretas. A resposta deve ser no formato: {question: '...', options: ['opção 0', 'opção 1', 'opção 2'], correct: 0}. Eu quero apenas o que está dentro de {} e não quero nada além disso.`
+                content: `Você é um criador de perguntas de múltipla escolha no estilo "Show do Milhão". Sua tarefa é elaborar perguntas em português do Brasil, com nível de dificuldade fácil ou médio, relacionadas ao tema "${randomTheme}". Essas perguntas serão usadas em um jogo familiar para pessoas de todas as idades. A pergunta deve ser simples, objetiva e clara. As opções de resposta devem ser curtas (no máximo 3 palavras), diretas, e não óbvias. Garanta que as respostas estejam corretamente relacionadas ao tema e que apenas uma seja correta. A saída deve seguir rigorosamente este formato: {question: '...', options: ['opção 0', 'opção 1', 'opção 2'], correct: 0}. Não envie nada além do conteúdo dentro das chaves.  `
+
             },
         ],
         model: "llama3-8b-8192",
